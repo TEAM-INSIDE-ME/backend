124307367837d7e0b440e82f84fbfe41b12d8090
Deld <deld4@icloud.com>
2024-07-06 16:05:52 +0900

    init

---
+++ src/main/java/com/insideme/insidemebackend/InsidemeBackendApplication.java
@@ -0,0 +1,13 @@
+package com.insideme.insidemebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InsidemeBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InsidemeBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/insideme/insidemebackend/InsidemeBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.insideme.insidemebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InsidemeBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
