05a55a26f746817ce334f4fc7f8573a263e56afb
Deld <deld4@icloud.com>
2024-08-09 13:27:17 +0900

    kakao login

--- src/main/java/com/insideme/insidemebackend/service/UserService.java
+++ src/main/java/com/insideme/insidemebackend/service/UserService.java
@@ -11,8 +11,8 @@ public class UserService {
     @Autowired
     private UserRepository userRepository;
 
-    public User saveUser(User user) {
-        return userRepository.save(user);
+    public void saveUser(User user) {
+        userRepository.save(user);
     }
 
 }
